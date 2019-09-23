bf945ea81f2f31cb7f9e471164a66215e4cd7663
LinNacume <54641442+LinNacume@users.noreply.github.com>
2019-09-23 20:42:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,6 +29,16 @@ $(function(){
  //    }, 400);
 });
 
+menu.onclick = function myFunction() {
+	var x = document.getElementById("myNavigation");
+
+	if (x.className === "navigation") {
+		x.className += " responsive";
+	} else{
+		x.className = "navigation";
+	}
+}
+
 // $(document).ready(function() {
 //     try {
 //         $('#water').ripples({
